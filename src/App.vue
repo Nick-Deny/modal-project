e23f446c427cd9c0696d00b99439f51ca1553158
Nick-Deny <NICKDENY5@HOTMAIL.COM>
2023-09-06 22:23:35 -0500

    Added modal
    Using a new component
    
    No closing functionality.. yet

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
  <h1>{{ title }}</h1>
+ <input type="text" ref="name">
+ <button @click="handleClick">click me</button>
+ <!-- 3) to use it in the template -->
+ <Modal />
 </template>
 
 <script>
+// 1) Need to import the modal
+import Modal from './components/Modal.vue'
+
 export default {
   name: 'App',
+  // 2) and register the modal as a component
+  components: { Modal },
   data() {
     return {
       title: 'My First Vue App :)'
+    }
+  },
+  methods: {
+    handleClick () {
+      // this - refers to component
+      // $refs - get access to any ref that we name
+      // name - access the ref named "name"
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
     }
   }
 }
@@ -24,7 +43,9 @@ export default {
 }
 h1 {
   border-bottom: 1px solid #ddd;
-  display: inline-block;
-  padding-bottom: 10px;
+  width: fit-content;
+  margin: 0 auto;
+  padding-bottom: 1rem;
+  margin-bottom: 1rem;
 }
 </style>
